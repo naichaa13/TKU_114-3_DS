c9a4ddb5d6a0b950b366ab948d1211d2c69fe067
許家禎 <>
2026-07-03 23:25:30 +0800

    Add 0702 Java practice

---
+++ 0702/CourseInfo.java
@@ -0,0 +1,13 @@
+public class CourseInfo {
+    public static void main(String[] args) {
+        String courseName = "Introduction to Java";
+        int credits = 3;
+        double hours = 2.5;
+        boolean required = true;
+
+        System.out.println("Name: " + courseName);
+        System.out.println("Credits: " + credits);
+        System.out.println("Hours: " + hours);
+        System.out.println("Required: " + required);
+    }
+}
